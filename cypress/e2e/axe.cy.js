c4020928d51e41bc589541fa50f86e77786db1a5
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-11-26 12:35:19 +0000

    refactor wording and pages in axe and fixtures specs

--- cypress/e2e/axe.cy.js
+++ cypress/e2e/axe.cy.js
@@ -2,13 +2,13 @@ import { terminalLog } from '../support/axe'
 
 describe('Axe check', () => {
   it('should have no detectable a11y violations on load', () => {
-    cy.visit('accessibility/test-card.html')
+    cy.visit('https://p-n-c.github.io/website/accessibility/test-card.html')
     cy.injectAxe()
     cy.checkA11y(null, null, terminalLog)
   })
 
   it('should have lots of detectable a11y violations on load', () => {
-    cy.visit('accessibility/bad-test-card.html')
+    cy.visit('https://p-n-c.github.io/website/accessibility/bad-test-card.html')
     cy.injectAxe()
     cy.checkA11y(null, null, terminalLog)
   })
--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -3,20 +3,17 @@ describe('Environment check', () => {
     cy.intercept('/colours', { fixture: 'colours' }).as('getColours')
     cy.visit('/')
     cy.wait('@getColours').then(async (interception) => {
-      for (const [key, value] of Object.entries(interception.response)) {
-        cy.log(`${key}: ${value}`)
-      }
       const colours = interception.response.body
       assert.isNotNull(
         interception.response.body,
-        'Call to API to get colours succeeds'
+        'call to API to get colours succeeds'
       )
       assert.strictEqual(
         colours.length,
         4,
-        'Call to API to get 4 colours succeeds'
+        'call to API to get 4 colours succeeds'
       )
-      expect(colours.length).equal(4)
+      expect(colours.length, 'returned colours').equal(4)
     })
   })
 })
